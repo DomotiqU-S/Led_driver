d05abd924a0dbe597a20b7ecef8c95658896a34a
anthopop123 <72212667+anthopop123@users.noreply.github.com>
2024-02-22 15:37:53 -0500

    fonctionnel

--- main/main.cpp
+++ main/main.cpp
@@ -6,26 +6,21 @@ extern "C" void app_main()
     while (true)
     {
         ESP_LOGI("LED", "LED ON");
-        led_driver->switchState(true);
+        led_driver->switchState(false);
         led_driver->setIntensity(100);
-        ESP_LOGI("LED","setTemperature(2700)");
-        led_driver->setTemperature(2700);
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-        ESP_LOGI("LED","setTemperature(6500)");
+        ESP_LOGI("LED", "setTemperature(6500)");
         led_driver->setTemperature(6500);
+        ESP_LOGI("LED", "setIntensity 10");
+        led_driver->setIntensity(90);
         vTaskDelay(5000 / portTICK_PERIOD_MS);
-        ESP_LOGI("LED","setIntensity(10)");
-        led_driver->setIntensity(10);
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-        ESP_LOGI("LED","setIntensity(50)");
+        ESP_LOGI("LED", "setIntensity 50");
         led_driver->setIntensity(50);
         vTaskDelay(5000 / portTICK_PERIOD_MS);
-        ESP_LOGI("LED","setIntensity(100)");
-        led_driver->setIntensity(100);
+        ESP_LOGI("LED", "setIntensity 100");
+        led_driver->setIntensity(0);
         vTaskDelay(5000 / portTICK_PERIOD_MS);
         ESP_LOGI("LED", "LED OFF");
-        led_driver->switchState(false);
+        led_driver->switchState(true);
         vTaskDelay(5000 / portTICK_PERIOD_MS);
-        
     }
 }
